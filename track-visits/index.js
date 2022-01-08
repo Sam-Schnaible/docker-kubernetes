67bd3890e64ac6302c1b99f1779bc662958e5cd2
Sam Schnaible II <sschnaible83@gmail.com>
2022-01-07 18:35:43 -0800

    configure docker-compose file

--- track-visits/index.js
+++ track-visits/index.js
@@ -10,7 +10,7 @@ const client = redis.createClient({
 client.set('visits', 0);
 
 app.get('/', (req, res) => {
-  process.exit(0); //status code 0 means we exited and everything is OK
+  process.exit(2); //status code 0 means we exited and everything is OK
   client.get('visits', (err, visits) => {
     res.send(`Number of visits is ${visits}`);
     client.set('visits', parseInt(visits) + 1);
